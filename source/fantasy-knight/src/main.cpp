e619531c7302e333a47ed08f989dd57c95e118ce
laqieer <laqieer@126.com>
2022-02-02 18:19:50 +0800

    第4章示例代码

--- source/fantasy-knight/src/main.cpp
+++ source/fantasy-knight/src/main.cpp
@@ -1,33 +1,96 @@
 #include "bn_core.h"
 #include "bn_math.h"
+#include "bn_keypad.h"
 #include "bn_sprite_ptr.h"
 #include "bn_sprite_double_size_mode.h"
 #include "bn_sprite_items_fantasy_knight.h"
 
+bn::sprite_ptr *sprite;
+
+void init_sprite();
+void handle_user_input();
+
 int main()
 {
     bn::core::init();
-    bn::sprite_ptr sprite = bn::sprite_items::fantasy_knight.create_sprite(0, 0);
-    bn::sprite_ptr sprite_horizontal_flipped = bn::sprite_items::fantasy_knight.create_sprite(0, 0);
-    bn::sprite_ptr sprite_vertical_flipped = bn::sprite_items::fantasy_knight.create_sprite(0, 0);
-    bn::sprite_ptr sprite_scaled = bn::sprite_items::fantasy_knight.create_sprite(0, 0);
-    bn::sprite_ptr sprite_rotated_45 = bn::sprite_items::fantasy_knight.create_sprite(0, 0);
-    bn::sprite_ptr sprite_rotated_90 = bn::sprite_items::fantasy_knight.create_sprite(0, 0);
-    sprite.set_position(-120 + 16, 80 - 32);
-    sprite_horizontal_flipped.set_position(sprite.x() + 32, 80 - 32);
-    sprite_vertical_flipped.set_position(sprite_horizontal_flipped.x() + 32, 80 - 32 + 64 - 40);
-    sprite_scaled.set_position(sprite_vertical_flipped.x() + 32, 80 - 32 * 2);
-    sprite_rotated_45.set_position(sprite_scaled.x() + 32, 80 - 32);
-    sprite_rotated_90.set_position(sprite_rotated_45.x() + 32, 80 - 32);
-    sprite_horizontal_flipped.set_horizontal_flip(true);
-    sprite_vertical_flipped.set_vertical_flip(true);
-    sprite_scaled.set_scale(2);
-    sprite_rotated_45.set_rotation_angle(45);
-    sprite_rotated_90.set_rotation_angle(90);
-    sprite_rotated_90.set_double_size_mode(bn::sprite_double_size_mode::ENABLED);
+    init_sprite();
 
     while(true)
     {
+        handle_user_input();
         bn::core::update();
+    }
+}
+
+void init_sprite()
+{
+    sprite = new bn::sprite_ptr(bn::sprite_items::fantasy_knight.create_sprite(0, 0));
+    sprite->set_double_size_mode(bn::sprite_double_size_mode::ENABLED);
+}
+
+void handle_user_input()
+{
+    // Start键: 复位
+    if(bn::keypad::start_pressed())
+    {
+        sprite->set_position(0, 0);
+        sprite->set_horizontal_flip(false);
+        sprite->set_vertical_flip(false);
+        sprite->set_scale(1);
+        sprite->set_rotation_angle(0);
+    }
+
+    // 十字键向上: 上移
+    if(bn::keypad::up_held() && sprite->y() > -80)
+    {
+        sprite->set_y(sprite->y() - 1);
+    }
+
+    // 十字键向下: 下移
+    if(bn::keypad::down_held() && sprite->y() < 80)
+    {
+        sprite->set_y(sprite->y() + 1);
+    }
+
+    // 十字键向左: 左移
+    if(bn::keypad::left_held() && sprite->x() > -120)
+    {
+        sprite->set_x(sprite->x() - 1);
+    }
+
+    // 十字键向右: 右移
+    if(bn::keypad::right_held() && sprite->x() < 120)
+    {
+        sprite->set_x(sprite->x() + 1);
+    }
+
+    // L键: 水平翻折
+    if(bn::keypad::l_pressed())
+    {
+        sprite->set_horizontal_flip(!sprite->horizontal_flip());
+    }
+
+    // R键: 垂直翻折
+    if(bn::keypad::r_pressed())
+    {
+        sprite->set_vertical_flip(!sprite->vertical_flip());
+    }
+
+    // A键: 放大
+    if(bn::keypad::a_held() && sprite->horizontal_scale() < 2)
+    {
+        sprite->set_scale(sprite->horizontal_scale() + 0.1);
+    }
+
+    // B键: 缩小
+    if(bn::keypad::b_held() && sprite->horizontal_scale() > 0.1)
+    {
+        sprite->set_scale(sprite->horizontal_scale() - 0.1);
+    }
+
+    // Select键: 旋转
+    if(bn::keypad::select_held())
+    {
+        sprite->set_rotation_angle((sprite->rotation_angle().integer() + 5) % 360);
     }
 }
