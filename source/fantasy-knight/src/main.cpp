4cd86b5e9d24b050a6f777ab8a3cf24b8e6b6d90
laqieer <laqieer@126.com>
2022-02-04 11:21:35 +0800

    第5章示例代码

--- source/fantasy-knight/src/main.cpp
+++ source/fantasy-knight/src/main.cpp
@@ -4,16 +4,29 @@
 #include "bn_sprite_ptr.h"
 #include "bn_sprite_double_size_mode.h"
 #include "bn_sprite_items_fantasy_knight.h"
+#include "bn_regular_bg_ptr.h"
+#include "bn_regular_bg_items_futurism_background.h"
+#include "bn_affine_bg_ptr.h"
+#include "bn_affine_bg_items_mystical_symbol.h"
 
 bn::sprite_ptr *sprite;
+bn::regular_bg_ptr *regular_bg;
+bn::affine_bg_ptr *affine_bg;
 
 void init_sprite();
+void init_regular_bg();
+void init_affine_bg();
+void reset_sprite();
+void reset_regular_bg();
+void reset_affine_bg();
 void handle_user_input();
 
 int main()
 {
     bn::core::init();
     init_sprite();
+    init_regular_bg();
+    init_affine_bg();
 
     while(true)
     {
@@ -26,6 +39,42 @@ void init_sprite()
 {
     sprite = new bn::sprite_ptr(bn::sprite_items::fantasy_knight.create_sprite(0, 0));
     sprite->set_double_size_mode(bn::sprite_double_size_mode::ENABLED);
+    sprite->set_bg_priority(0);
+}
+
+void init_regular_bg()
+{
+    regular_bg = new bn::regular_bg_ptr(bn::regular_bg_items::futurism_background.create_bg(0, 0));
+}
+
+void init_affine_bg()
+{
+    affine_bg = new bn::affine_bg_ptr(bn::affine_bg_items::mystical_symbol.create_bg(0, 0));
+    affine_bg->set_priority(2);
+    affine_bg->set_wrapping_enabled(false);
+}
+
+void reset_sprite()
+{
+    sprite->set_position(0, 0);
+    sprite->set_horizontal_flip(false);
+    sprite->set_vertical_flip(false);
+    sprite->set_scale(1);
+    sprite->set_rotation_angle(0);
+}
+
+void reset_regular_bg()
+{
+    regular_bg->set_y(0);
+}
+
+void reset_affine_bg()
+{
+    affine_bg->set_position(0, 0);
+    affine_bg->set_horizontal_flip(false);
+    affine_bg->set_vertical_flip(false);
+    affine_bg->set_scale(1);
+    affine_bg->set_rotation_angle(0);
 }
 
 void handle_user_input()
@@ -33,64 +82,70 @@ void handle_user_input()
     // Start键: 复位
     if(bn::keypad::start_pressed())
     {
-        sprite->set_position(0, 0);
-        sprite->set_horizontal_flip(false);
-        sprite->set_vertical_flip(false);
-        sprite->set_scale(1);
-        sprite->set_rotation_angle(0);
+        reset_sprite();
+        reset_regular_bg();
+        reset_affine_bg();
     }
 
-    // 十字键向上: 上移
-    if(bn::keypad::up_held() && sprite->y() > -80)
+    // 十字键向上: 场景
+    if(bn::keypad::up_held() && regular_bg->y() < 48)
     {
-        sprite->set_y(sprite->y() - 1);
+        sprite->set_y(sprite->y() + 1);
+        affine_bg->set_y(sprite->y());
+        regular_bg->set_y(regular_bg->y() + 1);
     }
 
     // 十字键向下: 下移
     if(bn::keypad::down_held() && sprite->y() < 80)
     {
         sprite->set_y(sprite->y() + 1);
+        affine_bg->set_y(sprite->y());
     }
 
     // 十字键向左: 左移
     if(bn::keypad::left_held() && sprite->x() > -120)
     {
         sprite->set_x(sprite->x() - 1);
+        affine_bg->set_x(sprite->x());
     }
 
     // 十字键向右: 右移
     if(bn::keypad::right_held() && sprite->x() < 120)
     {
         sprite->set_x(sprite->x() + 1);
+        affine_bg->set_x(sprite->x());
     }
 
     // L键: 水平翻折
     if(bn::keypad::l_pressed())
     {
         sprite->set_horizontal_flip(!sprite->horizontal_flip());
+        affine_bg->set_horizontal_flip(sprite->horizontal_flip());
     }
 
     // R键: 垂直翻折
     if(bn::keypad::r_pressed())
     {
-        sprite->set_vertical_flip(!sprite->vertical_flip());
+        affine_bg->set_vertical_flip(!affine_bg->vertical_flip());
     }
 
     // A键: 放大
     if(bn::keypad::a_held() && sprite->horizontal_scale() < 2)
     {
         sprite->set_scale(sprite->horizontal_scale() + 0.1);
+        affine_bg->set_scale(sprite->horizontal_scale());
     }
 
     // B键: 缩小
     if(bn::keypad::b_held() && sprite->horizontal_scale() > 0.1)
     {
         sprite->set_scale(sprite->horizontal_scale() - 0.1);
+        affine_bg->set_scale(sprite->horizontal_scale());
     }
 
     // Select键: 旋转
     if(bn::keypad::select_held())
     {
-        sprite->set_rotation_angle((sprite->rotation_angle().integer() + 5) % 360);
+        affine_bg->set_rotation_angle((affine_bg->rotation_angle().integer() + 5) % 360);
     }
 }
