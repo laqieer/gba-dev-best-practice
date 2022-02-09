9828ca12639b05de54cabbd301e5c75e998c5c6e
laqieer <laqieer@126.com>
2022-02-09 23:06:32 +0800

    common English text

--- source/common-text/src/main.cpp
+++ source/common-text/src/main.cpp
@@ -1,8 +1,18 @@
 #include "bn_core.h"
+#include "bn_math.h"
+#include "bn_sprite_ptr.h"
+#include "bn_sprite_text_generator.h"
+
+#include "common_variable_8x16_sprite_font.h"
+
+#define MAX_SPRITES 32
 
 int main()
 {
     bn::core::init();
+
+    bn::sprite_text_generator *text_en_generator = new bn::sprite_text_generator(common::variable_8x16_sprite_font);
+    bn::vector<bn::sprite_ptr, MAX_SPRITES> text_sprites = text_en_generator->generate<MAX_SPRITES>(-120, -64, "This is an English text.");
 
     while(true)
     {
